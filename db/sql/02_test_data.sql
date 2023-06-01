1cbf4155e565b7219b5d77af99aed0c3b2f35f09
nzjacl <jacquiallen1@gmail.com>
2023-06-01 14:23:30 +1200

    fixed all sql issues from proj 1 feedback

--- db/sql/02_test_data.sql
+++ db/sql/02_test_data.sql
@@ -1,3 +1,3 @@
-INSERT INTO form (policyNumber, customerIdNumber, condition, firstSymptoms, symptomDetails, serviceType, providerFacility, alternativeHealthInsurance, consentStatement, claimStatus)
+INSERT INTO claims (policyNumber, customerIdNumber, condition, firstSymptoms, symptomDetails, serviceType, providerFacility, alternativeHealthInsurance, consentStatement, claimStatus)
 VALUES (78945612, 123, 'broken leg', '20230330', 'pain', 'x-ray', 'Pacific Radiology: Wanaka', FALSE, TRUE, 'submitted');
 
