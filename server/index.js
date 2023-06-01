9a6061b1e9ab968a286c32c265e725a30b6707d0
nzjacl <jacquiallen1@gmail.com>
2023-06-01 14:29:22 +1200

    commented out form router - will remove shortly

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const formRouter = require("./routes/form-router");
+// const formRouter = require("./routes/form-router");
 const claimsRouter = require("./routes/claims-router");
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/form", formRouter);
+// app.use("/api/form", formRouter);
 app.use("/api/claims", claimsRouter);
 
 const thePort = process.env.EXPRESS_PORT || 5000;
--- server/routes/form-router.js
+++ server/routes/form-router.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const pool = require("../db");
-// const { auth } = require('express-oauth2-jwt-bearer');
-const formRouter = express.Router();
+// const express = require("express");
+// const pool = require("../db");
+// // const { auth } = require('express-oauth2-jwt-bearer');
+// const formRouter = express.Router();
 
 
-// const jwtCheck = auth({
-//     audience: 'https://ensure-api.com',
-//     issuerBaseURL: 'https://dev-8a2dkllk1a5kywvs.us.auth0.com/',
-//     tokenSigningAlg: 'RS256'
-//   });
+// // const jwtCheck = auth({
+// //     audience: 'https://ensure-api.com',
+// //     issuerBaseURL: 'https://dev-8a2dkllk1a5kywvs.us.auth0.com/',
+// //     tokenSigningAlg: 'RS256'
+// //   });
 
 
 
-formRouter.get("/", async (req, res) => {
-    console.log('hot reloaded!')
-    try {
-        const allItems = await pool.query("SELECT * FROM form");
-        res.json(allItems.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-});
+// formRouter.get("/", async (req, res) => {
+//     console.log('hot reloaded!')
+//     try {
+//         const allItems = await pool.query("SELECT * FROM form");
+//         res.json(allItems.rows);
+//     } catch (err) {
+//         console.error(err.message);
+//     }
+// });
 
 
-module.exports = formRouter;
+// module.exports = formRouter;
