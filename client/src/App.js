690e5467dc6773e218f2a7d5aa0c97608f2c2bc8
nzjacl <jacquiallen1@gmail.com>
2023-06-09 23:55:38 +1200

    finished footer and privacy statement link

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
 import logo from "./ensure-logo.svg";
 // import LogInButton from "./components/LogInButton";
@@ -6,17 +8,22 @@ import logo from "./ensure-logo.svg";
 //components
 
 import InputForm from "./components/InputForm";
+import Footer from "./components/Footer";
+import Privacy from "./components/Privacy";
 
 function App() {
   return (
     <>
-    {/* <div>
+      {/* <div>
       <LogInButton />
     </div> */}
       <div className="container">
-        
         <img className="logo-img" src={logo} alt="ensure logo" />
-        <InputForm />
+        <Routes>
+          <Route exact path="/privacy-policy" element={<Privacy />}></Route>
+          <Route exact path="/claims-form" element={<InputForm />}></Route>
+        </Routes>
+        <Footer />
       </div>
     </>
   );
