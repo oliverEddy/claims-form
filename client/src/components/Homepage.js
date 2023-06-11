eae8e005cf2eb00d4cb35a65788dc5820a80872f
nzjacl <jacquiallen1@gmail.com>
2023-06-12 10:04:58 +1200

    first part of home page saved

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,26 @@
+// import homeImg from "./ensure-logo.svg";
+
+const Homepage = () => {
+  return (
+    <>
+      <h1>Keeping the unexpected, uncomplicated</h1>
+      <img src="./client-img/ensurepic.jpg" alt="pic of healthy active family" />
+      <p>Leading the industry in Trust, Wellbeing, Safety, Compassion</p>
+      <br></br>
+      <p>
+        At enSURE we believe every person has the right to insurance that is
+        easy to understand, will cover them for what they need, and is easiy
+        available when they need it.{" "}
+      </p>
+      <br></br>
+      <p>
+        Our Advisors are professionals with specialist knowledge to assist you
+        in selecting and tailoring your cover to suit your individual
+        circumstances. All of our staff are experienced and trained to best help
+        and support you in your time of need.
+      </p>
+    </>
+  );
+};
+
+export default Homepage;
