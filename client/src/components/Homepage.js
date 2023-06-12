f9cbdc29de632132dcdfbc282fc4bbdb164483ab
nzjacl <jacquiallen1@gmail.com>
2023-06-12 13:32:23 +1200

    img displaying now - need to fix styling&login

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,16 +1,19 @@
+import "./Homepage.css";
+import LogInButton from "./LogInButton";
 // import homeImg from "./ensure-logo.svg";
 
 const Homepage = () => {
   return (
     <>
+      <div className="home-container">
       <h1>Keeping the unexpected, uncomplicated</h1>
-      <img src="./client-img/ensurepic.jpg" alt="pic of healthy active family" />
+      <img src="ensurepic.jpg" alt="pic of healthy active family" className="home-pic"/>
       <p>Leading the industry in Trust, Wellbeing, Safety, Compassion</p>
       <br></br>
       <p>
         At enSURE we believe every person has the right to insurance that is
         easy to understand, will cover them for what they need, and is easiy
-        available when they need it.{" "}
+        available when they need it.
       </p>
       <br></br>
       <p>
@@ -19,6 +22,8 @@ const Homepage = () => {
         circumstances. All of our staff are experienced and trained to best help
         and support you in your time of need.
       </p>
+      <button className="login-button">{LogInButton}Log In</button>
+      </div>
     </>
   );
 };
