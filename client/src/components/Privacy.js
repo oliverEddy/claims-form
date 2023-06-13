df342f86959f6b7e4b1de03b35a0142e6f30a85d
nzjacl <jacquiallen1@gmail.com>
2023-06-13 17:10:45 +1200

    changed the spacing slightly on privacy policy

--- client/src/components/Privacy.js
+++ client/src/components/Privacy.js
@@ -8,23 +8,17 @@ const Privacy = () => {
         <p>
           We collect personal information from you, including information about
           your: name, contact information, claim information.
-        </p>
-        <br />
-        <p>
-          We collect your personal information in order to: process and assess
+</p>
+          <p>We collect your personal information in order to: process and assess
           claims.
-        </p>
-        <br />
-        <p>
-          Providing some information is optional. If you choose not to enter
+</p>
+          <p>Providing some information is optional. If you choose not to enter
           access to personal data, we'll be unable to adequately assess claims.
           You have the right to ask for a copy of any personal information we
           hold about you, and to ask for it to be corrected if you think it is
           wrong.
-        </p>
-        <br />
-        <p>
-          If you’d like to ask for a copy of your information, or to have it
+</p>
+          <p>If you’d like to ask for a copy of your information, or to have it
           corrected, please contact us at admin@ensure.com, or 09 468 8976, or
           151 Ponsonby Road, Auckland.
         </p>
