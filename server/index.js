35554f26b67ac3c1540409565ce944a5aaf477be
nzjacl <jacquiallen1@gmail.com>
2023-06-15 23:22:51 +1200

    merge conflict fix

--- server/index.js
+++ server/index.js
@@ -5,25 +5,19 @@ const app = express();
 import cors from "cors";
 import claimsRouter from "./routes/claims-router.js";
 import claimantRouter from "./routes/claimant-router.js";
-const cors = require("cors");
-const { auth } = require('express-oauth2-jwt-bearer');
-const claimsRouter = require("./routes/claims-router");
-const claimantRouter = require("./routes/claimant-router");
+const { auth } = require("express-oauth2-jwt-bearer");
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-
 const checkJwt = auth({
-  audience: 'https://ensure-api.com',
-  issuerBaseURL: 'https://dev-8a2dkllk1a5kywvs.us.auth0.com/',
-  tokenSigningAlg: 'RS256'
+  audience: "https://ensure-api.com",
+  issuerBaseURL: "https://dev-8a2dkllk1a5kywvs.us.auth0.com/",
+  tokenSigningAlg: "RS256",
 });
 
-
-
-app.use(checkJwt)
+app.use(checkJwt);
 
 app.use("/api/claims", claimsRouter);
 
