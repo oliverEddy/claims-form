bd4627f579f1ed313677e40a4392f33421c50c72
olle1134 <99058610+olle1134@users.noreply.github.com>
2023-06-15 12:27:29 +1200

    Add file LogInzButtton.test, there is a test that tests the login button displays correctly to the client

---
+++ client/src/components/LogInButton.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { useAuth0 } from "@auth0/auth0-react";
+import LogInButton from "./LogInButton";
+import "@testing-library/jest-dom/extend-expect"; // Add this line
+
+jest.mock("@auth0/auth0-react", () => ({
+  useAuth0: jest.fn(),
+}));
+
+describe("LogInButton", () => {
+  test("renders login button correctly", () => {
+    const loginWithRedirectMock = jest.fn();
+    useAuth0.mockReturnValue({ loginWithRedirect: loginWithRedirectMock });
+
+    render(<LogInButton />);
+
+    const loginButton = screen.getByRole("button", { name: /log in/i });
+    expect(loginButton).toBeInTheDocument();
+
+    fireEvent.click(loginButton);
+    expect(loginWithRedirectMock).toHaveBeenCalledTimes(1);
+  });
+});
